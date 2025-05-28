33d9210c1200af0ed9ac0c84fad3cbf73bf992f9
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2025-05-29 00:45:24 +0900

    wip: edit CMake

--- cmake/ElfInterp.cmake
+++ cmake/ElfInterp.cmake
@@ -1,18 +1,30 @@
 
-find_program(OBJCOPY_EXECUTABLE "objcopy")
-if(OBJCOPY_EXECUTABLE)
-	set(ELF_INTERP_BIN ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp)
-	set(ELF_INTERP_SRC ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp.c)
-	set(ELF_INTERP_DAT ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp.dat)
+if(CMAKE_CROSSCOMPILING)
+	# クロスコンパイル時は、ターゲットシステムのELFインタープリターを推定
+	if(CMAKE_SYSTEM_PROCESSOR MATCHES "i.86")
+		set(ELF_INTERP "/lib/ld-linux.so.2")
+	elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "x86_64")
+		set(ELF_INTERP "/lib64/ld-linux-x86-64.so.2")
+	else()
+		set(ELF_INTERP "/lib/ld-linux.so.2")  # fallback
+	endif()
+	message(STATUS "Cross-compiling: Using target ELF interpreter: ${ELF_INTERP}")
+else()
+	find_program(OBJCOPY_EXECUTABLE "objcopy")
+	if(OBJCOPY_EXECUTABLE)
+		set(ELF_INTERP_BIN ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp)
+		set(ELF_INTERP_SRC ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp.c)
+		set(ELF_INTERP_DAT ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/elf_interp.dat)
 
-	file(WRITE ${ELF_INTERP_SRC} "int main(int argc, char **argv) { return 0; }")
+		file(WRITE ${ELF_INTERP_SRC} "int main(int argc, char **argv) { return 0; }")
 
-	execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_FLAGS} -o ${ELF_INTERP_BIN} ${ELF_INTERP_SRC})
-	execute_process(COMMAND ${OBJCOPY_EXECUTABLE} -O binary --only-section=.interp ${ELF_INTERP_BIN} ${ELF_INTERP_DAT})
+		execute_process(COMMAND ${CMAKE_C_COMPILER} ${CMAKE_C_FLAGS} -o ${ELF_INTERP_BIN} ${ELF_INTERP_SRC})
+		execute_process(COMMAND ${OBJCOPY_EXECUTABLE} -O binary --only-section=.interp ${ELF_INTERP_BIN} ${ELF_INTERP_DAT})
 
-	if(EXISTS ${ELF_INTERP_DAT})
-		file(READ ${ELF_INTERP_DAT} ELF_INTERP)
-		string(STRIP ${ELF_INTERP} ELF_INTERP)
+		if(EXISTS ${ELF_INTERP_DAT})
+			file(READ ${ELF_INTERP_DAT} ELF_INTERP)
+			string(STRIP ${ELF_INTERP} ELF_INTERP)
+		endif()
 	endif()
 endif()
 

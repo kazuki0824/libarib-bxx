614c2970dc83c18506576587c928481426617365
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2026-08-17 20:05:13 +0900

    Dispatch Multi2 through NEON when enabled

--- aribb25/multi2_cipher.h
+++ aribb25/multi2_cipher.h
@@ -194,7 +194,7 @@ inline void decrypt_cbc_ofb(uint8_t *buf, size_t n, const iv_type &iv, const wor
 		decrypt_block<x86::xmm>(buf, n, state, key, round);
 	}
 
-#elif defined(__ARM_NEON__) || defined(__ARM_NEON)
+#elif defined(LIBARIBB25_USE_NEON)
 	if (MULTI2_LIKELY(n == 184)) {
 		decrypt_block<arm::neon2<7> >(buf, n, state, key, round);
 		decrypt_block<arm::neon2<8> >(buf, n, state, key, round);
@@ -219,7 +219,7 @@ inline void decrypt_cbc_ofb(uint8_t *buf, size_t n, const iv_type &iv, const wor
 		memset(&t[n], 0,   8 - n);
 
 		block<uint32_t> c;
-		c.load(&t[0]);
+		c.load(buf);
 
 		block<uint32_t> p = c ^ cipher<uint32_t>::encrypt(state, key, round);
 		p.store(&t[0]);
